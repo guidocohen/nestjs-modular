3df8f622746e98307ca28a400c9485e36731efd5
Guido <guidocohens@gmail.com>
2024-01-31 23:44:00 -0300

    Env config & validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { lastValueFrom } from 'rxjs';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { HttpModule, HttpService } from '@nestjs/axios';
-
-const API_KEY = '123456789';
-const API_KEY_PROD = 'PROD12121212SA';
+import { enviroments } from 'environments';
+import { env } from 'process';
+import config from './config';
 
 @Module({
-  imports: [UsersModule, ProductsModule, HttpModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: enviroments[env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_PORT: Joi.number().default(8090),
+      }),
+    }),
+    UsersModule,
+    ProductsModule,
+    HttpModule,
+  ],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
     {
       provide: 'TASKS',
       useFactory: async (http: HttpService) => {
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,5 +1,5 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { User } from '../entities/user.entity';
 import { Order } from '../entities/order.entity';
 import { CreateUserDto, UpdateUserDto } from '../dtos/user.dto';
@@ -8,7 +8,10 @@ import { ProductsService } from './../../products/services/products.service';
 
 @Injectable()
 export class UsersService {
-  constructor(private productsService: ProductsService) {}
+  constructor(
+    private productsService: ProductsService,
+    private configServcie: ConfigService,
+  ) {}
 
   private counterId = 1;
   private users: User[] = [
@@ -21,6 +24,10 @@ export class UsersService {
   ];
 
   findAll() {
+    const apiKey = this.configServcie.get('API_KEY');
+    console.log('🚀 ~ UsersService ~ findAll ~ apiKey:', apiKey);
+    const dbName = this.configServcie.get('DB_NAME');
+    console.log('🚀 ~ UsersService ~ findAll ~ dbName:', dbName);
     return this.users;
   }
 
